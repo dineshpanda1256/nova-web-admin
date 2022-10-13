ea2b651b8f410a4499b074a5ec1e0256d62d5a74
Dinesh <dineshpanda1256@gmail.com>
2022-10-13 11:29:41 +0530

    admin-panel-nova

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Dashboardcomp from '../../components/Dashboardcomp/Dashboardcomp'
 
 export default function Dashboard() {
   return (
     <div>
-        Dashboard
+        <Dashboardcomp/>
     </div>
   )
 }
